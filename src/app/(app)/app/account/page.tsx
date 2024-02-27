c923fb0749565093e64a826a7ad25093985ef514
Will Price <pricew86@gmail.com>
2024-02-27 21:01:35 +0800

    add account page layout

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,3 +1,14 @@
+import ContentBlock from '@/components/content-block';
+import H1 from '@/components/h1';
+
 export default function AccountPage() {
-  return <div>AccountPage</div>;
+  return (
+    <main>
+      <H1 className='text-white my-8'>Your Account</H1>
+
+      <ContentBlock className='flex justify-center items-center h-[500px]'>
+        <p>Logged in as ...</p>
+      </ContentBlock>
+    </main>
+  );
 }
