81888b99a9872ca049b198555dd667224b3b6f0d
Will Price <pricew86@gmail.com>
2024-02-29 12:34:42 +0800

    server-side validations with zod

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -4,13 +4,24 @@ import { revalidatePath } from 'next/cache';
 import { sleep } from '@/lib/utils';
 import { PetEssentials } from '@/lib/types';
 import { Pet } from '@prisma/client';
+import { PetFormSchema, PetIdSchema } from '@/lib/validations';
 
-export async function addPet(pet: PetEssentials) {
+export async function addPet(pet: unknown) {
+  //unknown is a safer type for a backend endpoint instead of assuming the data will be the correct type
   await sleep(1000)
+
+  const validatedPet = PetFormSchema.safeParse(pet);
+
+  if (!validatedPet.success) {
+    return {
+      message: "Invalid pet data."
+    }
+  }
+
   try {
 
     await prisma.pet.create({
-       data: pet
+       data: validatedPet.data
     })
   } catch (error) {
     return {
@@ -22,14 +33,26 @@ export async function addPet(pet: PetEssentials) {
  revalidatePath('/app', "layout");
 }
 
-export async function editPet(petId: Pet['id'], newPetData: PetEssentials) {
+export async function editPet(petId: unknown, newPetData: unknown) {
   await sleep(1000)
+
+  const validatedPetId = PetIdSchema.safeParse(petId);
+  
+  const validatedPet = PetFormSchema.safeParse(newPetData);
+
+  if (!validatedPet.success || !validatedPetId.success) {
+    return {
+      message: "Invalid pet data."
+    }
+  }
+
+
   try {
     await prisma.pet.update({
       where: {
-        id: petId,
+        id: validatedPetId.data,
       },
-      data: newPetData
+      data: validatedPet.data
     });
   }
   catch (error) {
@@ -41,12 +64,20 @@ export async function editPet(petId: Pet['id'], newPetData: PetEssentials) {
 
 }
 
-export async function deletePet(petId: Pet['id']) {
+export async function deletePet(petId: unknown) {
   await sleep(1000)
+
+  const validatedPetId = PetIdSchema.safeParse(petId);
+
+  if (!validatedPetId.success) {
+    return {
+      message: "Invalid pet data."
+    }
+  }
   try {
     await prisma.pet.delete({
       where: {
-        id: petId,
+        id: validatedPetId.data,
       },
     });
   }
--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -6,30 +6,16 @@ import { Label } from './ui/label';
 import { Textarea } from './ui/textarea';
 import PetFormBtn from './pet-form-btn';
 import { useForm } from 'react-hook-form';
-import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { PetFormSchema, TPetForm } from '@/lib/validations';
 
 type PetFormProps = {
   actionType: 'add' | 'edit';
   onFormSubmission: () => void;
 };
 
-const PetFormSchema = z.object({
-  name: z.string().trim().min(1, { message: 'Name is required' }).max(20),
-  ownerName: z
-    .string()
-    .trim()
-    .min(1, { message: 'Owner name is required' })
-    .max(50),
-  imageUrl: z.union([
-    z.literal(''),
-    z.string().trim().url({ message: 'Invalid image url' }),
-  ]),
-  age: z.coerce.number().int().positive().max(99999),
-  notes: z.union([z.literal(''), z.string().trim().max(1000)]),
-});
-
-type TPetForm = z.infer<typeof PetFormSchema>;
+//none of the transformations (even .trim and .coerce) will occur with server actions and getValues() will return the original data
+//NOTE: The above statement is incorrect. The transformations will through validations on the server. The getValues() will return the original data, but the data that is sent to the server will be transformed.
 
 export default function PetForm({
   actionType,
@@ -40,29 +26,36 @@ export default function PetForm({
   const {
     register,
     trigger,
-    clearErrors,
+    getValues,
     formState: { errors },
   } = useForm<TPetForm>({
     mode: 'onBlur',
     resolver: zodResolver(PetFormSchema),
+    defaultValues:
+      actionType === 'edit'
+        ? {
+            name: selectedPet?.name,
+            ownerName: selectedPet?.ownerName,
+            imageUrl: selectedPet?.imageUrl,
+            age: selectedPet?.age,
+            notes: selectedPet?.notes,
+          }
+        : undefined,
   });
 
   return (
     <form
-      action={async (formData) => {
+      action={async () => {
         const result = await trigger();
+        //needed to trigger validation without using a submithandler
         if (!result) return;
+
         onFormSubmission();
 
-        const petData = {
-          name: formData.get('name') as string,
-          imageUrl:
-            (formData.get('imageUrl') as string) ||
-            'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png',
-          age: parseInt(formData.get('age') as string),
-          notes: formData.get('notes') as string,
-          ownerName: formData.get('ownerName') as string,
-        };
+        const petData = getValues();
+        // petData.imageUrl = petData.imageUrl || DEFAULT_PET_IMAGE;
+        // this is needed if not validating on the server
+
         if (actionType === 'add') {
           await handleAddPet(petData);
         } else if (actionType === 'edit' && selectedPet) {
