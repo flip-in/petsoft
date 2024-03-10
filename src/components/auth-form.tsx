0b6cd9baa37f1bdc7c3b53d141f461a14fa1b349
Will Price <pricew86@gmail.com>
2024-03-10 19:49:15 +0800

    auth form accept type prop

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -2,7 +2,11 @@ import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 
-export default function AuthForm() {
+type AuthFormProps = {
+  type: 'signup' | 'login';
+};
+
+export default function AuthForm({ type }: AuthFormProps) {
   return (
     <form className=''>
       <div className='space-y-1'>
@@ -13,7 +17,7 @@ export default function AuthForm() {
         <Label htmlFor='password'>Password</Label>
         <Input id='password' type='password' />
       </div>
-      <Button type='submit'>Log in</Button>
+      <Button type='submit'>{type === 'signup' ? 'Sign up' : 'Log in'}</Button>
     </form>
   );
 }
