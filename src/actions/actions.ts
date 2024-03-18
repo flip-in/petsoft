266b7f576063df638147c230cf012b05ec44ef04
Will Price <pricew86@gmail.com>
2024-03-18 10:48:28 -0500

    validations for sign up

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -21,12 +21,30 @@ export async function logIn(formData: unknown) {
   await signIn('credentials', formData)
 }
 
-export async function signUp(formData: FormData) {
-  const hashedPassword = await bcrypt.hash(formData.get('password') as string, 10)
+export async function signUp(formData: unknown) {
+  //check if formData is a FormData type
+  if (!(formData instanceof FormData)) {
+    return {
+      message: "Invalid credentials."
+    }
+  }
+  // convert formData to javascript object
+  const formDataEntries = Object.fromEntries(formData.entries())
+
+  //validate the object
+  const validatedFormData = authSchema.safeParse(formDataEntries);
+  if (!validatedFormData.success) {
+    return {
+      message: "Invalid credentials."
+    }
+  }
+
+  const {email, password} = validatedFormData.data
+  const hashedPassword = await bcrypt.hash(password, 10)
 
   await prisma.user.create({
     data: {
-      email: formData.get('email') as string ,
+      email,
       hashedPassword
     }
   })
