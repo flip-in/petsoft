c71bf77221bde02aee4759dfeefdb026d651f166
Will Price <pricew86@gmail.com>
2024-03-19 19:24:42 -0500

    add stripe and checkoutSession

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -8,6 +8,9 @@ import bcrypt from 'bcryptjs';
 import { checkAuth, getPetById } from '@/lib/server-utils';
 import { Prisma } from '@prisma/client';
 import { AuthError } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // --- user actions ---
 
@@ -222,3 +225,25 @@ export async function deletePet(petId: unknown) {
   revalidatePath('/app', "layout");
 }
 
+// payment actions
+
+export async function createCheckoutSession() {
+  const session = await checkAuth();
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    customer_email: session.user.email,
+    line_items: [
+      {
+        price: 'price_1OwCAP01vfhd9kIPIAwldfjT',
+        quantity: 1,
+      }
+    ],
+    mode: 'payment',
+    success_url: `${process.env.CANONICAL_URL}/payment?success=true`,
+    cancel_url: `${process.env.CANONICAL_URL}/payment?cancelled=true`,
+
+  })
+
+  //redirect to stripe checkout
+  redirect(checkoutSession.url)
+}
