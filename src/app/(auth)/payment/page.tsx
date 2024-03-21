9e173ea526e696ebe81371392fb449dd5a7856de
Will Price <pricew86@gmail.com>
2024-03-21 17:40:03 -0500

    update jwt with hasAccess after payment

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,6 +3,8 @@
 import { createCheckoutSession } from '@/actions/actions';
 import H1 from '@/components/h1';
 import { Button } from '@/components/ui/button';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { useTransition } from 'react';
 
 export default function Page({
@@ -10,11 +12,24 @@ export default function Page({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  const router = useRouter();
   const [isPending, startTransition] = useTransition();
+  const { update } = useSession();
 
   return (
     <main className='flex flex-col items-center space-y-10'>
       <H1>Petsoft access requires payment</H1>
+
+      {searchParams.success && (
+        <Button
+          onClick={async () => {
+            await update(true);
+            router.push('/app/dashboard');
+          }}
+        >
+          Access PetSoft
+        </Button>
+      )}
 
       {!searchParams.success && (
         <Button
